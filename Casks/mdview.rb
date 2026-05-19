8b4b26356d1e8b0afc36dd41b068fef7caf849b1
Toni Väisänen <toni@tvaisanen.com>
2026-05-19 09:07:28 +0300

    Update mdview to v0.3.1

--- Casks/mdview.rb
+++ Casks/mdview.rb
@@ -1,6 +1,6 @@
 cask "mdview" do
-  version "0.3.0"
-  sha256 "8443c6c3d7581710e8f22e236f2fe1434e54d7720c0af3bd62482e4742d7b738"
+  version "0.3.1"
+  sha256 "eb5c280778ceb61902ddcbb2b7240784cdd778b2a8a7cfd0cf740eb760d50749"
 
   url "https://github.com/slantedt/sigla-dist/releases/download/v#{version}/MarkdownViewer.zip"
   name "MarkdownViewer"
